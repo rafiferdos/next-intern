7a8c5dead63c73b6a945e138fac324c2150a4f73
Rafi Ferdos <rafiferdos@gmail.com>
2024-09-26 20:54:39 +0600

    add Navbar component and integrate it into the layout

---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { Navbar, NavbarBrand, NavbarContent, NavbarMenuToggle } from "@nextui-org/navbar";
+import { useState } from "react";
+
+const Nav = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const navLinks = [
+        "Profile",
+        "Dashboard",
+        "Log Out",
+      ];
+
+    return (
+        <Navbar onMenuOpenChange={setIsMenuOpen}>
+            <NavbarContent>
+            <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <MainLogo />
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+            </NavbarContent>
+        </Navbar>
+    );
+};
+
+export default Nav;
