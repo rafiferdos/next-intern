a7f909c1faf791f789cd4f9357d08b867ef46372
Rafi Ferdos <rafiferdos@gmail.com>
2024-09-28 20:41:02 +0600

    add metadata for Features page with title and description

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,5 +1,10 @@
 import { Button } from "@nextui-org/button";
 
+export const metadata: Metadata = {
+    title: "Next Intern | Features",
+    description: 'Features of Next Intern including intern/job search, intern/job application, and more',
+  };
+
 const page = () => {
     return (
         <div>
