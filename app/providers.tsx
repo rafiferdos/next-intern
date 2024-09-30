bdf42d00c4d88fd8d7f94f1c2407b1f74f7b2621
Rafi Ferdos <rafiferdos@gmail.com>
2024-09-30 23:07:13 +0600

    add: wrap application in QueryClientProvider for React Query integration

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,23 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/system";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
+
+const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <ThemeProvider attribute="class" defaultTheme="dark" themes={['light', 'dark']}>
-        {children}
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          themes={["light", "dark"]}
+        >
+          {children}
+        </ThemeProvider>
+      </QueryClientProvider>
     </NextUIProvider>
   );
 }
