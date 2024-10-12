5fc2d9b535a6fa30eece094f1ca34b284c119a2b
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-12 17:19:47 +0600

    feat: enhance authentication by adding GitHub provider and email-based credentials

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,37 +1,37 @@
-import GoogleProvider from "next-auth/providers/google";
-import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import Credentials from "next-auth/providers/credentials";
+// import GoogleProvider from "next-auth/providers/google";
+// import { NextAuthOptions } from "next-auth";
+// import GithubProvider from "next-auth/providers/github";
+// import Credentials from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    Credentials({
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize() {
-        let user = null;
+// export const authOptions: NextAuthOptions = {
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID as string,
+//       clientSecret: process.env.GITHUB_SECRET as string,
+//     }),
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//     Credentials({
+//       credentials: {
+//         username: { label: "Username", type: "text" },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize() {
+//         let user = null;
 
-        user = {
-          id: "1",
-          name: "John Doe",
-          email: "rafi@gmail.com",
-        };
-        if (!user) {
-          console.log("Invalid credentials");
-          return null;
-        }
-        return user;
-      },
-    }),
-  ],
-};
+//         user = {
+//           id: "1",
+//           name: "John Doe",
+//           email: "rafi@gmail.com",
+//         };
+//         if (!user) {
+//           console.log("Invalid credentials");
+//           return null;
+//         }
+//         return user;
+//       },
+//     }),
+//   ],
+// };
