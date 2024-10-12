a0ea0cdbbc5ed149031835f470bb9f7098212896
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-12 16:59:20 +0600

    feat: implement credentials provider for enhanced authentication

--- auth.ts
+++ auth.ts
@@ -1,5 +1,27 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
-export const {handlers, signIn, signOut, auth}  = NextAuth({
-    providers: []
-})
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize() {
+        let user = null;
+
+        user = {
+          id: "1",
+          name: "John Doe",
+          email: "rafi@gmail.com",
+        };
+        if (!user) {
+          console.log("Invalid credentials");
+          return null;
+        }
+        return user;
+      },
+    }),
+  ],
+});
