2e32314492928097e55622a9876a090b9e71355f
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-13 13:26:44 +0600

    feat: integrate Prisma adapter for NextAuth and add bcrypt for password hashing

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,40 @@
-import { handlers } from "@/auth";
+import prisma from '@/lib/prisma';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { compare } from "bcrypt";
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials";
 
-export const { GET, POST } = handlers;
+export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		Credentials({
+			credentials: {
+				email: { label: "email", type: "email" },
+				password: { label: "password", type: "password" },
+			},
+			async authorize(credentials) {
+				const { email, password } = credentials ?? {};
+				if (!email || !password) {
+					throw new Error("email or password is missing");
+				}
+				const user = await prisma.user.findUnique({
+					where: {
+						email: email,
+					},
+				});
+				if (!user) {
+					throw new Error("User does not exist");
+				}
+				const isValidPassword = await compare(password, user.password);
+				if (!isValidPassword) {
+					throw new Error("Invalid password");
+				}
+				return user;
+			},
+		}),
+	],
+};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
