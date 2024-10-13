b53c7cd4f08aefb62a9258e0f529ee5c80a22210
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-13 14:00:07 +0600

    feat: implement middleware for token validation and redirect logic

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,16 @@
-export { auth as middleware } from "@/auth";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-};
+export async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  if (path === "/") {
+    return NextResponse.next();
+  }
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  if (!token && path === "/protected") {
+    return NextResponse.redirect(new URL("/login", req.url));
+  } else if (token && (path === "/login" || path === "/register")) {
+    return NextResponse.redirect(new URL("/protected", req.url));
+  }
+  return NextResponse.next();
+}
