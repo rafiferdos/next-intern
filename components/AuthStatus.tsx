2ca19d8e0db45dfcae8148b030ee8f8e9801be3b
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-13 14:03:59 +0600

    feat: add AuthStatus component to display user session information

---
+++ components/AuthStatus.tsx
@@ -0,0 +1,14 @@
+import { getServerSession } from "next-auth";
+
+const AuthStatus = async () => {
+	const session = await getServerSession();
+
+	return (
+		<div className="absolute w-full top-5 flex items-center justify-center">
+			{session && (
+				<p className="text-white text-xl">Signed in as {session.user?.email}</p>
+			)}
+		</div>
+	);
+};
+export default AuthStatus;
