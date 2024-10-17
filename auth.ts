7acde6888a477924e34789f230b9da92d4ffee5c
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-17 15:14:00 +0600

    feat: implement user registration with password hashing and enhance credential authorization

--- auth.ts
+++ auth.ts
@@ -26,20 +26,48 @@ export const {
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     Credentials({
-        async authorize(credentials) {
-            if (!credentials) {
-                return null;
-            }
-            const user = await db.user.findFirst({
-                where: {
-                    email: credentials.email,
-                },
-            });
-            if (user && (await bcrypt.compare(credentials.password, user.password))) {
-                return { id: user.id, email: user.email };
-            }
-            return null;
+        name: "Credentials",
+        credentials: {
+          email: {
+            label: "Email",
+            type: "email",
+            placeholder: "email@example.com",
+          },
+          password: { label: "Password", type: "password" },
         },
-    })
+        authorize: async (credentials) => {
+          if (!credentials || !credentials.email || !credentials.password) {
+            return null;
+          }
+  
+          const email = credentials.email as string;
+          const hash = saltAndHashPassword(credentials.password);
+  
+          let user: any = await db.user.findUnique({
+            where: {
+              email,
+            },
+          });
+  
+          if (!user) {
+            user = await db.user.create({
+              data: {
+                email,
+                hashedPassword: hash,
+              },
+            });
+          } else {
+            const isMatch = bcrypt.compareSync(
+              credentials.password as string,
+              user.hashedPassword
+            );
+            if (!isMatch) {
+              throw new Error("Incorrect password.");
+            }
+          }
+  
+          return user;
+        },
+      }),
   ],
 });
