6265ad110a3685229ea9820f5fe65a419e9105b6
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-17 14:42:40 +0600

    feat: enhance logout functionality to include redirect option and ensure path revalidation

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,11 +1,11 @@
-import { signIn, signOut } from "next-auth/react"
+import { signIn, signOut } from "next-auth/react";
 import { revalidatePath } from "next/cache";
 
 export const login = async (provider: string) => {
-    await signIn(provider, { redirectTo: "/" });
-    revalidatePath("/");
-}
+  await signIn(provider, { redirectTo: "/" });
+  revalidatePath("/");
+};
 export const logout = async () => {
-    await signOut();
-    revalidatePath("/");
-}
+  await signOut({ redirect: true });
+  revalidatePath("/");
+};
