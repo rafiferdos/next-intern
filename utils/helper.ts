60da488cb62ace998b8e09faac63372c6b6b5012
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-17 20:50:55 +0600

    feat: add JWT authentication support with jsonwebtoken and jwt-decode, remove unused helper function

--- utils/helper.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcryptjs";
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function saltAndHashPassword(password: any) {
-  const saltRounds = 10; // Adjust the cost factor according to your security requirements
-  const salt = bcrypt.genSaltSync(saltRounds); // Synchronously generate a salt
-  const hash = bcrypt.hashSync(password, salt); // Synchronously hash the password
-  return hash; // Return the hash directly as a string
-}
