248c2b8feda10d71778c69dce8a09f9d9c2f5aae
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-17 21:44:45 +0600

    refactor: simplify NextAuth configuration by removing unused providers and exporting handler directly

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,6 @@
+import { AuthOptions } from "@/config/nextauth.config";
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize() {
-        const user = {
-          id: "1",
-          name: "J Smith",
-          email: "jsmith@example.com",
-          role: "user",
-        };
+const handler = NextAuth(AuthOptions);
 
-        if (user) {
-          return user;
-        } else {
-          return null;
-        }
-      },
-    }),
-  ],
-};
-
-export default NextAuth(authOptions);
+export { handler as GET, handler as POST };
