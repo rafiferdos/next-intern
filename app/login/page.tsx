e1593c668d41cf9ea684334d55d6600529a3c94c
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-17 20:48:44 +0600

    feat: implement GitHub and credentials authentication providers, refactor login page, and enhance middleware for role-based access control

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,26 +1,21 @@
-import { LockKeyholeIcon } from "lucide-react";
-import Link from "next/link";
-import Form from "@/components/Form";
-import { Card } from "@nextui-org/card";
-import { Divider } from "@nextui-org/divider";
+'use client'
+import { signIn, signOut, useSession } from "next-auth/react"
 
-const LoginPage = () => {
-	return (
-		<div className="flex h-screen justify-center items-center">
-			<Card className="z-10 max-w-md w-full overflow-hidden rounded-2xl shadow-xl">
-				<div className="flex flex-col items-center justify-center space-y-3 py-6 text-center sm:px-16">
-					<Link href={"/"}>
-						<LockKeyholeIcon className="h-10 w-10" />
-					</Link>
-					<h3 className="text-xl font-semibold">Login</h3>
-					<p className="text-sm">
-						Sign in with your email and password
-					</p>
-				</div>
-				<Divider className="my-4" />
-				<Form type="login" />
-			</Card>
-		</div>
-	);
-};
-export default LoginPage;
+
+export default function Login() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,85 @@
-import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { decode } from "./utils/jwt.decode";
 
-export async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname;
-  if (path === "/") {
+interface TDecode {
+  _id: string;
+  name: string;
+  email: string;
+  mobileNumber: string;
+  profilePhoto: string;
+  role: "USER" | "ADMIN";
+  status: "ACTIVE";
+  iat: number;
+  exp: number;
+}
+
+const AuthRoutes = [
+  "/login",
+  "/register",
+  "/forget-password",
+  "/reset-password",
+];
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  // Check for access token
+  const accessToken = cookies().get("accessToken")?.value;
+
+  if (!accessToken) {
+    // If user is not logged in, allow access to auth routes only
+    if (AuthRoutes.includes(pathname)) {
+      return NextResponse.next(); // Allow access to login/register
+    } else {
+      // Redirect to login page with redirect parameter
+      return NextResponse.redirect(
+        new URL(`/login?redirect=${pathname}`, request.url)
+      );
+    }
+  }
+
+  // If user is logged in, decode token
+  const decodedToken = accessToken ? (decode(accessToken) as TDecode) : null;
+
+  // If there is a valid user and the role is detected
+  if (decodedToken?.role == "ADMIN" || decodedToken?.role == "USER") {
+    // Prevent logged-in users from accessing login/register routes
+    if (AuthRoutes.includes(pathname)) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    // Role-based authorization
+    if (decodedToken.role === "ADMIN" && pathname.match(/^\/admin-dashboard/)) {
+      return NextResponse.next(); // Allow access to admin dashboard
+    }
+
+    if (decodedToken.role === "USER" && pathname.match(/^\/dashboard/)) {
+      return NextResponse.next(); // Allow access to user dashboard
+    }
+    // Allow access to common routes for both USER and ADMIN
+
     return NextResponse.next();
   }
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  if (!token && path === "/protected") {
-    return NextResponse.redirect(new URL("/login", req.url));
-  } else if (token && (path === "/login" || path === "/register")) {
-    return NextResponse.redirect(new URL("/protected", req.url));
-  }
-  return NextResponse.next();
+
+  // If for some reason the token is invalid, redirect to login
+  return NextResponse.redirect(new URL("/login", request.url));
 }
+
+export const config = {
+  matcher: [
+    "/login",
+    "/register",
+    "/forget-password",
+    "/reset-password",
+    "/",
+    "/about-us",
+    "/contact-us",
+    "/posts",
+    "/subscription",
+    "/profile",
+    "/dashboard/:page*",
+    "/admin-dashboard/:page*",
+  ],
+};
