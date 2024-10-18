f2da0eb805b2fbd9b2c59e650eef66b2d5f80eb5
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-18 17:43:14 +0600

    feat: update authentication middleware and integrate new NextAuth version with improved user handling

--- app/contact_us/page.tsx
+++ app/contact_us/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@nextui-org/button';
 import React from 'react';
+import { auth } from "@/auth"
 
-const contact_us = () => {
+const contact_us = async () => {
+    const session = await auth()
+
     return (
         <div>
             <Button color='danger'>Contact Us</Button>
+            Info on user: {session?.user.name}
         </div>
     );
 };
--- auth.ts
+++ auth.ts
@@ -1,57 +1,7 @@
-import { NextAuthOptions } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 
-// Define the saltAndHashPassword function
-function saltAndHashPassword(password: string): string {
-  // Implement your salting and hashing logic here
-  return password; // Replace this with actual hashing logic
-}
-
-export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    Credentials({
-        // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-        // e.g. domain, username, password, 2FA token, etc.
-        credentials: {
-          email: {},
-          password: {},
-        },
-        authorize: async (credentials) => {
-          let user = null
-   
-          // logic to salt and hash password
-          if (!credentials) {
-            throw new Error("Credentials are missing.");
-          }
-          const pwHash = saltAndHashPassword(credentials.password);
-   
-          // logic to verify if the user exists
-          user = await getUserFromDb(credentials.email, pwHash);
-   
-          if (!user) {
-            // No user found, so this is their first attempt to login
-            // meaning this is also the place you could do registration
-            throw new Error("User not found.")
-          }
-   
-          // return user object with their profile data
-          return user
-        },
-      }),
-  ],
-//   pages: {
-//     signIn: "/login",
-//   },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+export const { auth, handlers, signIn, signOut } = NextAuth({
+ providers: [GitHub, Google],
+})
