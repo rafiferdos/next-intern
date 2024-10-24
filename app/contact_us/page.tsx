cc49d5199bc06f6886c13e4d85d0ba82e26aabcd
Rafi Ferdos <rafiferdos@gmail.com>
2024-10-24 22:47:00 +0600

    refactor: comment out unused imports and session logic in contact_us page

--- app/contact_us/page.tsx
+++ app/contact_us/page.tsx
@@ -1,17 +1,15 @@
-import { Button } from '@nextui-org/button';
+// import { Button } from '@nextui-org/button';
 import React from 'react';
-import { auth } from "@/auth"
-import Image from 'next/image';
+// import Image from 'next/image';
 
 const contact_us = async () => {
-    const session = await auth()
 
     return (
         <div>
-            <Button color='danger'>Contact Us</Button>
+            {/* <Button color='danger'>Contact Us</Button>
             Info on user: {session?.user.email}
             <Image src={session?.user?.image || ''} alt={session?.user?.name || 'User Image'} width={100} height={100} />
-            <div>{session?.user?.role}</div>
+            <div>{session?.user?.role}</div> */}
         </div>
     );
 };
